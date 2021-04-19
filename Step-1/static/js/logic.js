8401c1fe92e0211fc30f96a7ec545ab6b9ac32a5
nsommerf <nsommerf@gmail.com>
2021-04-19 18:27:57 -0500

    clean-up

--- Step-1/static/js/logic.js
+++ Step-1/static/js/logic.js
@@ -52,7 +52,6 @@ d3.json(url).then(function(response) {
   //Your data markers should reflect the magnitude of the earthquake by their size and and depth of the 
   //earthquake by color. Earthquakes with higher magnitudes should appear larger and earthquakes with 
   //greater depth should appear darker in color.
-  //response.features[i].geometry.coordinates[3])
   //Include popups that provide additional information about the earthquake when a marker is clicked.
 
   //Create a legend that will provide context for your map data.
